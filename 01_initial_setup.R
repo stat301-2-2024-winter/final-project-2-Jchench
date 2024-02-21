524d1d9c15a3eabfe8b4b9598e4056c51c9f9233
Jchench <jessiechen2026@u.northwestern.edu>
2024-02-21 12:55:53 -0600

    renaming

---
+++ 01_initial_setup.R
@@ -0,0 +1,12 @@
+# splitting dataset, folds
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(naniar)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load data
