30ec91c229012006aeeeb35175bad0f4b3482b18
Jchench <jessiechen2026@u.northwestern.edu>
2024-02-28 13:50:36 -0600

    started recipe 2

--- R Scripts/02_recipes.R
+++ R Scripts/02_recipes.R
@@ -46,11 +46,46 @@ baseline_recipe_trees <-
   step_zv(all_predictors()) |> 
   step_normalize(all_numeric_predictors())
 
-
-  # step_corr(all_predictors())
-
 # prep and bake
 prep(baseline_recipe_trees) |> 
+  bake(new_data = NULL)
+
+# Feature Engineered recipe ----
+
+# for parametric models
+main_recipe <- 
+  recipe(target ~ fathers_qualification + mothers_qualification + admission_grade +
+           previous_qualification_grade + gender + debtor + scholarship_holder +
+           curricular_units_1st_sem_grade + curricular_units_2nd_sem_grade,
+         data = drop_out_train) |> 
+  step_dummy(all_nominal_predictors()) |>
+  step_interact(~ admission_grade:previous_qualification_grade) |> 
+  step_interact(~ starts_with("fathers_qualification"):starts_with("mothers_qualification")) |> 
+  step_interact(~ curricular_units_1st_sem_grade:curricular_units_2nd_sem_grade) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+# prep and bake
+
+main_recipe_prep_bake <- 
+  prep(main_recipe) |> 
+  bake(new_data = NULL)
+
+# for tree-based models
+main_recipe_trees <- 
+  recipe(target ~ fathers_qualification + mothers_qualification + admission_grade +
+           previous_qualification_grade + gender + debtor + scholarship_holder +
+           curricular_units_1st_sem_grade + curricular_units_2nd_sem_grade,
+         data = drop_out_train) |> 
+  step_dummy(all_nominal_predictors()) |>
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors()) |> 
+  step_corr(all_predictors())
+
+# step_corr(all_predictors())
+
+trees_recipe_prep_bake <- 
+  prep(main_recipe_trees) |> 
   bake(new_data = NULL)
 
 # save out recipes ----
